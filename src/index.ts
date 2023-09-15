75539dd472f1d68412d798236b2728cdfb187c2a
foomoon <daniel.j.claxton@gmail.com>
2023-09-15 15:07:03 -0500

    restructure folders

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// imports
+import { Led } from "./lib/led.js";
+import { Frame } from "./lib/frame.js";
+import { Movie } from "./lib/movie.js";
+import { Light, AuthenticationToken } from "./lib/light.js";
+import {
+  rgbColor,
+  hsvColor,
+  deviceMode,
+  applicationResponseCode,
+  timer,
+} from "./lib/interfaces.js";
+import { discoverTwinklyDevices, TwinklyDevice } from "./lib/discovery.js";
+
+export {
+  Led,
+  Frame,
+  Movie,
+  Light,
+  AuthenticationToken,
+  rgbColor,
+  hsvColor,
+  deviceMode,
+  applicationResponseCode,
+  timer,
+  TwinklyDevice,
+  discoverTwinklyDevices,
+};
