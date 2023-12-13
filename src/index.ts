d03e00901db9ea5d00560d42adebb7b36b556b53
foomoon <daniel.j.claxton@gmail.com>
2023-12-12 21:58:29 -0600

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
-// imports
-import { Led } from "./lib/led.js";
-import { Frame } from "./lib/frame.js";
-import { Movie } from "./lib/movie.js";
-import { Light, AuthenticationToken } from "./lib/light.js";
-import {
-  rgbColor,
-  hsvColor,
-  deviceMode,
-  applicationResponseCode,
-  timer,
-} from "./lib/interfaces.js";
-import { discoverTwinklyDevices, TwinklyDevice } from "./lib/discovery.js";
-
+export { Led } from "./lib/led.js";
+export { Frame } from "./lib/frame.js";
+export { Movie } from "./lib/movie.js";
+export { Light, AuthenticationToken } from "./lib/light.js";
 export {
-  Led,
-  Frame,
-  Movie,
-  Light,
-  AuthenticationToken,
   rgbColor,
   hsvColor,
   deviceMode,
   applicationResponseCode,
   timer,
-  TwinklyDevice,
-  discoverTwinklyDevices,
-};
+  coordinate,
+  layout,
+} from "./lib/interfaces.js";
+export { TwinklyDevice, discoverTwinklyDevices } from "./lib/discovery.js";
